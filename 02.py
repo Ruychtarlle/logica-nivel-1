4777873cd837626c60e85cb5d7dc08f280554fd8
Ruychtarlle <ruychtarlle@hotmail.com>
2025-03-13 19:43:48 -0300

    Update 02.py

--- 02.py
+++ 02.py
@@ -1,3 +1,4 @@
+# Elabore um programa que imprima seu nome na primeira linha, o estado em que você mora na segunda, e seu município na terceira
 print("ruychtarlle pereira barbosa")
 print("rio de janeiro")
 print("rio de janeiro")
