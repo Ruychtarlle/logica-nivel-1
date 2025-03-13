54781812d63e443a5b8708b7fad60b5ffd18968c
Ruychtarlle <ruychtarlle@hotmail.com>
2025-03-13 20:51:04 -0300

    Create alogoritimo.py

---
+++ alogoritimo.py
@@ -0,0 +1,8 @@
+#  Faça um alogoritimo para somar dois numeros, observação vale para divisão, soma. muitiplicão e porcentagem 
+print("Qual primeiro numero?")
+numero1 = int(input() )
+print("qual segundo numero?")
+numero2 = int(input() )
+soma = numero1 + numero2
+print(soma)
+
