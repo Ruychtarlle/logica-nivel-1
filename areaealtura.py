3283377c18711c6268fab2cc7dc94ab83b753af4
Ruychtarlle <ruychtarlle@hotmail.com>
2025-03-13 22:01:58 -0300

    Create areaealtura.py

---
+++ areaealtura.py
@@ -0,0 +1,7 @@
+# Escrever um algoritimo para ler as dimensões de um retangulo(base e altura, calcular e escrever a area do retangulo)
+
+base =int(input("Qual a base?"))
+altura =int(input("Qual a altura?"))
+resultado = base * altura
+print("a area é:" ,resultado)
+
