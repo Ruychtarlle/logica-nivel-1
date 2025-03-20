68c97e2dbd1fab02be99a05bf4a4d00f280afcbf
Ruychtarlle <ruychtarlle@hotmail.com>
2025-03-20 19:36:32 -0300

    Update and rename 10 to 010.py

---
+++ 010.py
@@ -0,0 +1,9 @@
+ # Ler um valor e escrever se é positivo ou negativo (considere o valor zero como positivo
+print("qual numero estou digitando?")
+input("")
+if(10):
+    print("Esse valor de 10 é positivo")
+else:
+    print("Esse valor é negativo")
+    
+
