ca2d11232711fb833eaf8e10cee5249b58b5193e
Ruychtarlle <ruychtarlle@hotmail.com>
2025-03-20 19:15:06 -0300

    Update 09.py

--- 09.py
+++ 09.py
@@ -1,1 +1,6 @@
+ # Ler um valor e escrever a mensagem É MAIOR QUE 10! se o valor lido for maior que 10, caso contrário escrever NÃO É MAIOR QUE 10!
 
+if(10 > 7):
+    print("10 é maior que 7")
+else:
+    print("5 não é maior que 7")
