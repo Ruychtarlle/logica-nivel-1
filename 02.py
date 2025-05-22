d5a01bebeda8424e28b74b529671b507d95604b9
Ruychtarlle <ruychtarlle@hotmail.com>
2025-05-22 19:30:19 -0300

    Update and rename 02.py to Exercício_02.py
    2. Etiqueta - Elabore um programa que imprima seu nome na primeira linha, o estado em que você mora na segunda, e seu município na terceira.

--- 02.py
+++
@@ -1,4 +0,0 @@
-# Elabore um programa que imprima seu nome na primeira linha, o estado em que você mora na segunda, e seu município na terceira
-print("ruychtarlle pereira barbosa")
-print("rio de janeiro")
-print("rio de janeiro")
---
+++ Exercício_02.py
@@ -0,0 +1,16 @@
+#2. Etiqueta - Elabore um programa que imprima seu nome na primeira linha, o estado em que você mora na segunda, e seu município na terceira.
+
+# Nome
+nome = input("Digite seu nome?")
+
+#Estado
+estado = input("Digite seu estado?")
+
+#Municipio
+municipio = input("Digite seu municipio?")
+
+# imprimir o que foi pedido escrito
+print(nome)
+print(estado)
+print(municipio)
+
