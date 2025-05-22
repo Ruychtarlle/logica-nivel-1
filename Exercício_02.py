8de4b58d6983f7256a21d0edad9f96eaca3f1e4a
Ruychtarlle <ruychtarlle@hotmail.com>
2025-05-22 19:56:13 -0300

    Update Exercício_02.py

--- Exercício_02.py
+++ Exercício_02.py
@@ -1,16 +1,15 @@
 #2. Etiqueta - Elabore um programa que imprima seu nome na primeira linha, o estado em que você mora na segunda, e seu município na terceira.
+#Solicitar e armazenar o nome do usuário
+nome = input("Digite seu nome: ")
 
-# Nome
-nome = input("Digite seu nome?")
+# Solicitar e armazenar o estado do usuário
+estado = input("Digite seu estado: ")
 
-#Estado
-estado = input("Digite seu estado?")
+# Solicitar e armazenar o município do usuário
+municipio = input("Digite seu município: ")
 
-#Municipio
-municipio = input("Digite seu municipio?")
-
-# imprimir o que foi pedido escrito
-print(nome)
-print(estado)
-print(municipio)
-
+# Imprimir as informações de forma formatada
+print("\nInformações do Usuário:")
+print(f"Nome: {nome}")
+print(f"Estado: {estado}")
+print(f"Município: {municipio}")
