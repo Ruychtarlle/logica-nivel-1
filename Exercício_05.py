e6701aa7329c00558c01059ec3fc2353d8784ea5
Ruychtarlle <ruychtarlle@hotmail.com>
2025-05-27 19:01:25 -0300

    Update and rename 05.py to Exercício_05.py
    Tabela de notas - Você foi contratado por uma escola pra fazer o sistema de boletim dos alunos. Como primeiro passo, escreva um programa que produza a seguinte saída

--- Exercício_05.py
+++ Exercício_05.py
@@ -1,7 +1,12 @@
 # Tabela de notas - Você foi contratado por uma escola pra fazer o sistema de boletim dos alunos. Como primeiro passo, escreva um programa que produza a seguinte saída
+
+# Lista de alunos e notas
+listadealuno = ("Aline", 9),("Mario", 9),("Sergio", 9),("Shirley", 9)
+
+# Imprimir cabeçalho
 print ("ALUNO(A)          NOTA")
 print("========          ====")
-print("Aline              9")
-print("mario              9")
-print("Sergio             9")
-print("Shirley            9")
+
+# Imprimir cada aluno e nota
+for listadealuno, nota in listadealuno:
+    print(f"{listadealuno:<17} {nota}")
