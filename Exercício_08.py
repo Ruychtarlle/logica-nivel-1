e5219fd4a6a375613e943067876272dbb5dc6504
Ruychtarlle <ruychtarlle@hotmail.com>
2025-05-27 19:20:37 -0300

    Update and rename 08.py to Exercício_08.py
    Implemente um programa que na primeira linha faça um print perguntando o nome do usuário e, na linha 2, chame a função input() para capturar o valor do teclado e armazenar na memória do computador.

--- Exercício_08.py
+++ Exercício_08.py
@@ -1,6 +1,7 @@
 #  Implemente um programa que na primeira linha faça um print perguntando o nome do usuário e, na linha 2, chame a função input() para capturar o valor do teclado e armazenar na memória do computador.
 
-print("qual seu nome?")
-input("")
+print("Qual é o seu nome?")
 
+nomedousuario = input()
 
+print(f"Olá,{nomedousuario}! Bem-vindo(a) ao programa XTZ.com.br.")
