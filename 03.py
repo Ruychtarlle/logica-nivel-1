de47c01dd6cb27b4ca39be4b8e25b379e7192b88
Ruychtarlle <ruychtarlle@hotmail.com>
2025-05-27 18:45:01 -0300

    Update and rename 03.py to Exercício_03.py
    Letra de música - Faça um programa que imprima na tela o nome de uma música que você gosta (proibido letras do Justin Bieber)

--- 03.py
+++
@@ -1,3 +0,0 @@
-# Letra de música - Faça um programa que imprima na tela o nome de uma música que você gosta (proibido letras do Justin Bieber)
-print("preciso que mostre a letra da musica do o rappa que mais gosto")
-print("A minha alma - O rappa")
---
+++ Exercício_03.py
@@ -0,0 +1,6 @@
+# Letra de música - Faça um programa que imprima na tela o nome de uma música que você gosta (proibido letras do Justin Bieber)
+musica = "Bohemian Rhapsody"
+cantor = "Queen"
+
+print("Qual é a música que mais gosto?", musica)
+print("Qual é o nome da banda?", cantor)
